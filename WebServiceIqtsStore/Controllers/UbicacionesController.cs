7414e1a2a661d8933fe0092b3fe76c78874a04fe
paredes09 <111245800+paredes09@users.noreply.github.com>
2024-09-09 00:19:04 -0500

    ADD STARTUP, GETCONNETION AND APPLICATIONDBCONTEXT NEW CONTROLADORES AND ENTIDADES, CONFIGURACION DEL PROGRAM

---
+++ WebServiceIqtsStore/Controllers/UbicacionesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebServiceIqtsStore.Controllers
+{
+    [ApiController]
+    [Route("api/ubicaciones")]
+    public class UbicacionesController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+        public UbicacionesController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        [HttpGet]
+        [Route("departamentos")]
+        public async Task<ActionResult> getDepartamentos()
+        {
+            var departamentos = await context.Departamentos.FromSqlRaw("SELECT * FROM DEPARTAMENTO").ToListAsync();
+            return Ok(departamentos);
+        }
+        [HttpGet]
+        [Route("provincias")]
+        public async Task<ActionResult> getProvincias()
+        {
+            var provincias = await context.Provincias.FromSqlRaw("SELECT * FROM PROVINCIA").ToListAsync();
+            return Ok(provincias);
+        }
+        [HttpGet]
+        [Route("distritos")]
+        public async Task<ActionResult> getDistritos()
+        {
+            var distritos = await context.Distritos.FromSqlRaw("SELECT * FROM DISTRITO").ToListAsync();
+            return Ok(distritos);
+        }
+
+
+    }
+}
